44a58cb256b4220cfb77ce7f396c94479bd92e74
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2021-03-27 18:15:48 +0700

    feat: Add security

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const passport = require('passport')
 
 const { mongoose } = require('./configs/mongodb')
 
+const helmet = require('helmet') //จัดการเกี่ยวกับ security ** ลบ power-by ได้ด้วย  จะลบ header express ออกไป
+const rateLimit = require('express-rate-limit') //จัดการเกี่ยวกับ request
+const  cors = require('cors')  //ให้ domain หรือ ip อื่น สามารถใช้ ip เราได้   *** django มีด้วย
+
 require('dotenv').config() //import .env
-
-
 
 const app = express()
 const server = require('http').createServer(app)
@@ -18,12 +20,30 @@ mongoose //connect mongoDb
 //initialize passport
 passport.initialize()
 
+//security
+app.use(helmet())
+
+//limit การยิง add  **ถ้าใช้ heroku เปิด  app.set('trust proxy', 1); ด้วย
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1)
+
+const limiter = rateLimit({
+    windowMs: 10 * 1000, // 10 วินาที
+    max: 5, // limit each IP to 100 requests per windowMs
+})
+//จำกัด 5 req ภายใน 10 วิ
+//  apply to all requests
+app.use(limiter);
+
+
 app.use(
     cors({
         origin: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
     })
 )
+
 if (process.env.NODE_ENV === 'production') {
     app.use(logger('dev'))
 }
@@ -34,8 +54,6 @@ app.use(
 )
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
-
-
 
 const menuRouter = require('./routes/menuRoute')
 app.use('/menu', menuRouter)
@@ -48,7 +66,7 @@ server.listen(port, () => {
     console.log(`server running on ${port}`)
 })
 //import middleware  **ต้อง use ไว้ล่างสุด ก่อน export
-const {errorValidator} = require('./middleware/errorHandler')
+const { errorValidator } = require('./middleware/errorHandler')
 
 app.use(errorValidator)
 module.exports = app
